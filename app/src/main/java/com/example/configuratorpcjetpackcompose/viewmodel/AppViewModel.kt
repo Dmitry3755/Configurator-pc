17c874c164c5e3587301cf3aafdda01023417eab
Dmitry3755 <dmitry3755@gmail.com>
2022-10-23 23:07:50 +0300

    Добавлена библиотка ссопрограммы

--- app/src/main/java/com/example/configuratorpcjetpackcompose/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/example/configuratorpcjetpackcompose/viewmodel/AppViewModel.kt
@@ -3,8 +3,12 @@ package com.example.configuratorpcjetpackcompose.viewmodel
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.configuratorpcjetpackcompose.model.ConfiguratorRepository
 import com.example.configuratorpcjetpackcompose.services.ValidationService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers.IO
 
 class AppViewModel : ViewModel() {
 
@@ -21,6 +25,7 @@ class AppViewModel : ViewModel() {
                 repeatedPassword = repeatedPassword.value
             )
         ) {
+            viewModelScope.launch(Dispatchers.IO) {  }
             configuratorRepository.createUser(email.value, password.value)
         }
     }
